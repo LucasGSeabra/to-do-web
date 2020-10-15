e5768cc5eb9e2ffaea753e35ec30789d78db6b6b
Lucas Seabra <lucasgseabra@gmail.com>
2020-10-15 13:32:49 -0300

    Alterado arquivos para jsx

---
+++ src/main/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'font-awesome';
+import 'bootstrap'
+
+function App(props) {
+  return (
+    <div className="App">
+        <h1>Teste</h1>
+    </div>
+  );
+}
+
+export default App;

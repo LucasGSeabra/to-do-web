170b9afa0c9fa183ea1999f53739876298e7204c
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-10-17 18:04:28 -0300

    Adicionado Form em todoForm

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Col, FormControl, Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus }  from '@fortawesome/free-solid-svg-icons'
 
 function TodoForm(props) {
     return (
-
+        <form className="todoForm">
+            <Col xs={12} md={10} sm={9}>
+                <FormControl placeholder="Adicione uma tarefa" id="description" />                 
+            </Col>
+            <Col xs={12} md={2} sm={3}>
+                <Button variant="primary">
+                    <FontAwesomeIcon icon={faPlus} />
+                </Button>                 
+            </Col>
+        </form>
     )
 }
 

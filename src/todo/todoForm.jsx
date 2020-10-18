80558ebeedfa760f274f037930541d1d36b8ce75
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-10-18 12:11:19 -0300

    Adicionado eventos a TodoForm

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
-import { Col, FormControl, Button } from 'react-bootstrap'
+import { Row, Col, FormControl, Button } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus }  from '@fortawesome/free-solid-svg-icons'
 
 function TodoForm(props) {
     return (
         <form className="todoForm">
-            <Col xs={12} md={10} sm={9}>
-                <FormControl placeholder="Adicione uma tarefa" id="description" />                 
-            </Col>
-            <Col xs={12} md={2} sm={3}>
-                <Button variant="primary">
-                    <FontAwesomeIcon icon={faPlus} />
-                </Button>                 
-            </Col>
+            <Row>
+                <Col xs={12} md={10} sm={9}>
+                    <FormControl 
+                        value={props.description}
+                        onChange={props.handleChange}
+                        placeholder="Adicione uma tarefa" 
+                        id="description"
+                    />                 
+                </Col>
+                <Col xs={12} md={2} sm={3}>
+                    <Button onClick={props.handleAdd} variant="primary">
+                        <FontAwesomeIcon icon={faPlus} />
+                    </Button>                 
+                </Col>
+            </Row>    
         </form>
     )
 }

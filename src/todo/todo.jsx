27364154a79d45a8b2a76c994b493eae802c1ba6
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-10-18 11:33:32 -0300

    Refatorado Todo para função

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,16 +1,18 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+function Todo(props) {
 export default class Todo extends Component {
     render() {
-        return (
-            <div>
-                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+    return (
+        <div>
+            <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
                 <TodoForm></TodoForm>
                 <TodoList></TodoList>
-            </div>
-        )
-    }    
+        </div>
+    )
 }
+
+export default Todo 

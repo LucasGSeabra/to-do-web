528dd5469edd376aa7b2207897951086b0490b30
Lucas Seabra <lucasgseabra@gmail.com>
2020-10-22 13:35:49 -0300

    Adicionado tabela em todoList

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Button, Table } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash }  from '@fortawesome/free-solid-svg-icons'
 
 function TodoList(props) {
+
     return (
-        <div></div>
+        <Table>
+            <thead>
+                <tr>
+                    <th>Descricao</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.list.map(todo => {
+                    return (
+                        <tr key={todo.id}>
+                            <td>{todo.description}</td>
+                            <td>
+                                <Button onClick={props.handleDelete(todo.id)} variant="danger">
+                                    <FontAwesomeIcon icon={faTrash} />
+                                </Button> 
+                            </td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </Table>
     )
 }
 

3abf329fcaf28c3ee57191ac5966a35c7fbb5bed
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-11-02 11:17:30 -0300

    Criado Provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './main/App';
+import reducers from './main/reducers'
+
+const store = createStore(reducers)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('app')
 );

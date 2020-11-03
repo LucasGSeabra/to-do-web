9134f8cbad578af70992df201820f7d78da79c6a
Lucas Seabra <lucasgseabra@gmail.com>
2020-11-03 17:17:01 -0300

    Modificado todoReducer

--- src/todo/todoReducer.js
+++ src/todo/todoReducer.js
@@ -1,25 +1,16 @@
 const INITIAL_STATE = {
-    description: "Ler",
-    list: [{
-        _id: 1,
-        description: "Pagar fatura",
-        done: true
-    }, {
-        _id: 2,
-        description: "Reuniao",
-        done: false
-    },
-    {
-        _id: 3,
-        description: "Consulta",
-        done: false
-    }]
+    description: '',
+    list: []
 }
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case 'DESCRIPTION_CHANGED':
             return { ...state, description: action.payload }
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload.data }
+        case 'TODO_ADDED':
+            return { ...state, description: '' }
         default:
             return state
     }

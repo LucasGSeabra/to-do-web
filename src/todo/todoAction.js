ac1dd48d2c9f1763d7c936ace959173312da4ea0
Lucas Seabra <lucasgseabra@gmail.com>
2020-11-04 16:44:41 -0300

    Refatorado função add

--- src/todo/todoAction.js
+++ src/todo/todoAction.js
@@ -18,7 +18,7 @@ export const search = () => {
 export const add = (description) => {
     return dispatch => {
         axios.post(URL, { description })
-            .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data }))
+            .then(() => dispatch(clear()))
             .then(() => dispatch(search()))
     }
 }

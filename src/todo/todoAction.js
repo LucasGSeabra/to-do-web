92500a8bc243b7a438eff126f7ce345704f5d2e4
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-02-01 14:30:13 -0300

    Modificado URL

--- src/todo/todoAction.js
+++ src/todo/todoAction.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const URL = 'http://localhost:3003/api/todos'
+const URL = 'https://glacial-castle-07790.herokuapp.com/api/todos'
 
 export const changeDescription = (event) => ({
     type: 'DESCRIPTION_CHANGED',

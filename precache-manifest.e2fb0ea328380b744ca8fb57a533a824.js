f49293ace2971f23f5c30bb5e27e75a0c7d17edf
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-02-02 17:19:18 -0300

    Updates

---
+++ precache-manifest.e2fb0ea328380b744ca8fb57a533a824.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb5cf1b1ab8a33090086a1ca315a6ee",
+    "url": "/to-do-web/index.html"
+  },
+  {
+    "revision": "875235fad5479e339648",
+    "url": "/to-do-web/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "d7b956004f0b552f098a",
+    "url": "/to-do-web/static/css/main.df482300.chunk.css"
+  },
+  {
+    "revision": "875235fad5479e339648",
+    "url": "/to-do-web/static/js/2.c9c62c10.chunk.js"
+  },
+  {
+    "revision": "24bf724123f1af6c67bad1e94211dd14",
+    "url": "/to-do-web/static/js/2.c9c62c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b956004f0b552f098a",
+    "url": "/to-do-web/static/js/main.b45ea43d.chunk.js"
+  },
+  {
+    "revision": "37b1c6981042e0bd3456",
+    "url": "/to-do-web/static/js/runtime-main.33e462c9.js"
+  }
+]);

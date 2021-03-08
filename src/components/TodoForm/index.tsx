e580d7c3e25037a4a769e2308abc46b5256f7775
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-03-08 12:06:41 -0300

    Adicionado FormInput

--- src/components/TodoForm/index.tsx
+++ src/components/TodoForm/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import React, { KeyboardEvent } from 'react';
 import {
   Row, Col, FormControl, Button,
@@ -8,40 +9,15 @@ import { useAppDispatch, useAppSelector } from '../../store/types';
 import {
   descriptionChanged, descriptionCleared, fetchTodos, addTodos,
 } from '../../store/todoReducer';
+import FormInput from '../FormInput';
 import './styles.css';
 
 function TodoForm() {
-  const description = useAppSelector((state) => state.todo.description);
-  const dispatch = useAppDispatch();
-
-  function handleAdd() {
-    if (description.length > 0) {
-      dispatch(addTodos(description));
-    }
-  }
-
-  function keyHandler(e: KeyboardEvent) {
-    if (e.key === 'Enter') {
-      if (e.shiftKey) {
-        dispatch(fetchTodos(description));
-      } else {
-        handleAdd();
-      }
-    }
-    if (e.key === 'Escape') { dispatch(descriptionCleared()); }
-  }
-
   return (
     <form className="todoForm">
       <Row>
         <Col xs={12} md={10} sm={9}>
-          <FormControl
-            value={description}
-            onChange={(e) => dispatch(descriptionChanged(e.target.value))}
-            onKeyUp={(e: KeyboardEvent) => keyHandler(e)}
-            placeholder="Adicione uma tarefa"
-            id="description"
-          />
+          <FormInput />
         </Col>
         <Col xs={12} md={2} sm={3}>
           <Button onClick={() => handleAdd()} variant="primary">
